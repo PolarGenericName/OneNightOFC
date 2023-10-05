4d8992b2cad2d279a8542690bd29a51bd4391523
lucasolucas <115474698+lucasolucas@users.noreply.github.com>
2023-10-05 08:50:49 -0300

    IA de inimigo sendo feita

--- Assets/scripts/Inimigo_generico.cs
+++ Assets/scripts/Inimigo_generico.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Inimigo_generico : MonoBehaviour
@@ -11,16 +12,56 @@ public class Inimigo_generico : MonoBehaviour
     float velocidadeMovimento;
 
     [SerializeField]
+    private float distanciaMinima;
+
+    [SerializeField]
     private Rigidbody2D rigidbody2D;
 
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    //mecanica de raio de visão
+    [SerializeField]
+    private float raioDeVisao;
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(this.transform.position , this.raioDeVisao);
+    }
+    private void ProcurarJoagadorl()
+    {
+        Physics2D.OverlapCircle(this.transform.position, this.raioDeVisao);
+    }
 
     void Update()
     {
         Vector2 posicaoAlvo = this.alvo.position;
         Vector2 posicaoAtual = this.transform.position;
-        Vector2 direcao = posicaoAlvo - posicaoAtual;
-        direcao = direcao.normalized;
-        
-        this.rigidbody2D.velocity = (this.velocidadeMovimento * direcao);
-    }
+        //calcula distancia minima pro player
+        float distancia = Vector2.Distance(posicaoAtual, posicaoAlvo);
+
+        if (distancia >= this.distanciaMinima)
+        {
+            //movimenta inimigo
+            Vector2 direcao = posicaoAlvo - posicaoAtual;
+            direcao = direcao.normalized;
+
+            this.rigidbody2D.velocity = (this.velocidadeMovimento * direcao);
+            //flip de sprite
+            if (this.rigidbody2D.velocity.x > 0)
+            {
+                this.spriteRenderer.flipX = false;
+            }
+            else if (this.rigidbody2D.velocity.x < 0)
+            {
+                this.spriteRenderer.flipX = true;
+            }
+        }
+        else
+        {
+            this.rigidbody2D.velocity = Vector2.zero;
+        }
+   
+}
 }
